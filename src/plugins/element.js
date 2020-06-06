59ed3d0b2a05a5176377934c776ca3ac04ac3e75
在行走 <hsp_email@163.com>
2020-06-06 16:24:19 +0800

    完成了登录

---
+++ src/plugins/element.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message
+} from 'element-ui'
+
+// 绿色校验正确
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// Vue.use(Message)
+Vue.prototype.$message = Message
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '@/components/Login.vue'
+import Home from '@/components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', name: 'Login', component: Login },
+  { path: '/Login', name: 'Login', component: Login },
+  { path: '/home', name: 'home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守位
+// to 将要访问的路径
+// from 从哪个路径跳转来的
+// next 是一个函数表示放行
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
