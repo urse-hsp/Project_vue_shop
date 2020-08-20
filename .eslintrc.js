68396109d3e00683969455d4deda3f67390683f9
侯少平 <hsp_email@163.com>
2020-08-20 11:21:25 +0800

    修改esline,登录跳转更改用replace ,防止返回登录

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0
+    // https://blog.csdn.net/qq_43416014/article/details/105012478?utm_medium=distribute.pc_aggpage_search_result.none-task-blog-2~all~first_rank_v2~rank_v25-1-105012478.nonecase&utm_term=eslint%E8%AE%BE%E7%BD%AE%E5%87%BD%E6%95%B0%E7%A9%BA%E6%A0%BC
   }
 }
--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -2,9 +2,7 @@
     <div>
         <!-- 面包屑导航区 -->
         <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item :to="{ path: '/home' }"
-                >首页</el-breadcrumb-item
-            >
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
             <el-breadcrumb-item>订单管理</el-breadcrumb-item>
             <el-breadcrumb-item>订单列表</el-breadcrumb-item>
         </el-breadcrumb>
@@ -21,22 +19,22 @@
 
             <!-- 订单列表 -->
             <el-table :data="orderList" border stripe>
-                <el-table-column type='index'></el-table-column>
-                <el-table-column label='订单编号' prop='order_number'></el-table-column>
-                <el-table-column label='订单价格' prop='order_price'></el-table-column>
-                <el-table-column label='是否付款' prop='pay_status'>
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="订单编号" prop="order_number"></el-table-column>
+                <el-table-column label="订单价格" prop="order_price"></el-table-column>
+                <el-table-column label="是否付款" prop="pay_status">
                     <template slot-scope="scope">
-                        <el-tag type="success" v-if="scope.row.pay_status ===0">以付款</el-tag>
+                        <el-tag type="success" v-if="scope.row.pay_status === 0">以付款</el-tag>
                         <el-tag type="danger" v-else>未付款</el-tag>
                     </template>
                 </el-table-column>
-                <el-table-column label='是否发货' prop='is_send'></el-table-column>
-                <el-table-column label='下单时间'>
+                <el-table-column label="是否发货" prop="is_send"></el-table-column>
+                <el-table-column label="下单时间">
                     <template slot-scope="scope">
-                        {{scope.row.create_time | dataFormat}}
+                        {{ scope.row.create_time | dataFormat }}
                     </template>
                 </el-table-column>
-                <el-table-column label='操作'>
+                <el-table-column label="操作">
                     <template>
                         <el-button size="mini" type="primary" icon="el-icon-edit" @click="showBox"></el-button>
                         <el-button size="mini" type="success" icon="el-icon-location" @click="showProgressBox"></el-button>
@@ -45,24 +43,20 @@
             </el-table>
 
             <!-- 分页 -->
-                <el-pagination
-                    @size-change="handleSizeChange"
-                    @current-change="handleCurrentChange"
-                    :current-page="querInfo.pagenum"
-                    :page-sizes="[5, 10, 15]"
-                    :page-size="querInfo.pagesize"
-                    layout="total, sizes, prev, pager, next, jumper"
-                    :total="tabal">
-                </el-pagination>
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="querInfo.pagenum"
+                :page-sizes="[5, 10, 15]"
+                :page-size="querInfo.pagesize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="tabal"
+            >
+            </el-pagination>
         </el-card>
 
         <!-- 地址对话框 -->
-        <el-dialog
-            title="修改地址"
-            :visible.sync="ordessVisible"
-            width="50%"
-            @close='addressDialogClosed'
-        >
+        <el-dialog title="修改地址" :visible.sync="ordessVisible" width="50%" @close="addressDialogClosed">
             <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px">
                 <el-form-item label="省市区/县" prop="addrss1">
                     <el-cascader :options="cityData" v-model="addressForm.addrss1"></el-cascader>
@@ -78,22 +72,14 @@
         </el-dialog>
 
         <!-- 物流进度对话框 -->
-        <el-dialog
-            title="物流进度"
-            :visible.sync="ProgressVisible"
-            width="50%"
-        >
-                <!-- 时间线 -->
-              <el-timeline>
-                <el-timeline-item
-                    v-for="(activity, index) in progressInfo"
-                    :key="index"
-                    :timestamp="activity.time">
-                    {{activity.context}}
+        <el-dialog title="物流进度" :visible.sync="ProgressVisible" width="50%">
+            <!-- 时间线 -->
+            <el-timeline>
+                <el-timeline-item v-for="(activity, index) in progressInfo" :key="index" :timestamp="activity.time">
+                    {{ activity.context }}
                 </el-timeline-item>
             </el-timeline>
-            <span slot="footer" class="dialog-footer">
-            </span>
+            <span slot="footer" class="dialog-footer"> </span>
         </el-dialog>
     </div>
 </template>
@@ -117,12 +103,8 @@ export default {
             },
             // 验证规则
             addressFormRules: {
-                addrss1: [
-                    { required: true, message: '请选择省市区/县', trigger: 'blur' }
-                ],
-                addrss2: [
-                    { required: true, message: '请填写详细地址', trigger: 'blur' }
-                ]
+                addrss1: [{ required: true, message: '请选择省市区/县', trigger: 'blur' }],
+                addrss2: [{ required: true, message: '请填写详细地址', trigger: 'blur' }]
             },
             cityData,
             ProgressVisible: false,
